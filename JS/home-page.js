8a11a3035cc90ad78419c05d7a31f81fdc144255
Carlossantana987 <carlossantana987@outlook.com>
2019-05-22 01:42:11 -0400

    lalmost finshed with customer review section

---
+++ JS/home-page.js
@@ -0,0 +1,39 @@
+//menu toggle button
+const toggleMenu = () => {
+  menu.classList.toggle("menu--open");
+};
+const menu = document.querySelector(".menu");
+const menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener("click", toggleMenu);
+
+// customer comments script
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
