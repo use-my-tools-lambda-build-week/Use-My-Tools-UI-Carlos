f9bc116f04ba6ac6010e3b7edc5d858cc2ccc73c
Carlossantana987 <carlossantana987@outlook.com>
2019-05-22 14:40:33 -0400

    user review section done and working properly

--- JS/home-page.js
+++ JS/home-page.js
@@ -14,14 +14,9 @@ function plusSlides(n) {
   showSlides((slideIndex += n));
 }
 
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("Slides");
-  var dots = document.getElementsByClassName("dot");
   if (n > slides.length) {
     slideIndex = 1;
   }
@@ -31,9 +26,5 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
 }
