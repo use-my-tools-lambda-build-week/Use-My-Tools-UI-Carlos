ad1f75f46e22e472a3740e256f4f72d1bade7cc1
Carlossantana987 <carlossantana987@outlook.com>
2019-05-24 10:53:28 -0400

    final changes to landing page

--- JS/home-page.js
+++ JS/home-page.js
@@ -1,22 +1,34 @@
 // customer comments script
-var slideIndex = 1;
+let slideIndex = 100;
 slide(slideIndex);
 
-function plusSlides(n) {
-  slide((slideIndex += n));
-}
+const arrowClickPrev = document.querySelector(".prev");
+arrowClickPrev.addEventListener("click", function(n) {
+  slide((slideIndex += -1));
+});
+
+const arrowClickNext = document.querySelector(".next");
+arrowClickNext.addEventListener("click", function(n) {
+  slide((slideIndex += 1));
+});
 
 function slide(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
+  let i;
+  let slides = document.querySelectorAll(".slides");
   if (n > slides.length) {
     slideIndex = 1;
   }
   if (n < 1) {
     slideIndex = slides.length;
   }
+
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slides[slideIndex - 1].style.display = "block";
 }
+//events
+const mouseDown = document.querySelector(".btn");
+mouseDown.addEventListener("mousedown", function() {
+  mouseDown.style.background = "purple";
+});
